cb1815b809d3f62c299f4f559db9e44d099647df
AYUSHRAHUL <142750969+AYUSHRAHUL@users.noreply.github.com>
2025-06-06 04:13:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 const corsOptions = {
-  origin: ["https://job-khojo-frontend.vercel.app"],
+   origin: ["http://localhost:5173"],
   credentials: true,
 };
 
